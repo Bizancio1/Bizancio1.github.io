33d24d47bcb24b50716ddb7597b67c4fea516cce
Bizancio <joelbp5631@gmail.com>
2022-04-23 19:07:24 -0600

    removing the width printed on screen

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 
-let body = document.getElementById("body").offsetWidth;
-
-let h1 = document.getElementById("heigth");
-h1.innerText = body
- 
-
-
 function smoothScroll() {
 	document.getElementById("about").scrollIntoView({behavior: "smooth"});
 }
