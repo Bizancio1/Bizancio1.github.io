4bc3df5ed357f9f430ea6ece5a0a348522f88b98
Bizancio <joelbp5631@gmail.com>
2022-04-23 19:03:43 -0600

    looking the width of the body

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
+let body = document.getElementById("body").offsetWidth;
+
+let h1 = document.getElementById("heigth");
+h1.innerText = body
+ 
+
+
 function smoothScroll() {
 	document.getElementById("about").scrollIntoView({behavior: "smooth"});
 }
